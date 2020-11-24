9aa8138d3ed591306548d2c1644c5b637c3eeaaa
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2020-11-24 18:27:07 +0200

    Remove duplicated code

--- backend/src/jobs/scheduleCurrentChallengeComplition.ts
+++ backend/src/jobs/scheduleCurrentChallengeComplition.ts
@@ -5,15 +5,17 @@ import { User } from '../interfaces/user';
 import { getChallenge } from '../services/challenges/getChallenge/getChallenge';
 import { getUser } from '../services/users/getUser/getUser';
 import { addDaysToDate } from '../utils/addDaysToDate';
-import { completeCurrentTask } from '../services/tasks/completeCurrentTask/completeCurrentTask';
-import { StatusState } from '../constants/statusState';
+import { updateChallenge } from '../services/challenges/updateChallenge/updateChallenge';
+import { ChallengeState } from '../constants/challengeState';
 
 export function scheduleCurrentChallengeComplition(): void {
   const user: User = getUser();
   const challenge: Challenge = getChallenge(user.activeChallengeId);
   const challengeEndDate: Date = addDaysToDate(challenge.startDate, 30);
 
-  schedule.scheduleJob(challengeEndDate, function () {
-    completeCurrentTask(StatusState.Failure);
+  schedule.scheduleJob(challengeEndDate, () => {
+    const user: User = getUser();
+    const challenge: Challenge = getChallenge(user.activeChallengeId);
+    updateChallenge(challenge.id, { state: ChallengeState.Success });
   });
 }
